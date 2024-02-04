b17509334b1defd03af2ee1a47e6acd843a5416b
Carlos Aguilera <18aguilerac@gmail.com>
2024-02-04 01:50:38 +0000

    commenting

--- labs/lab4/lab4.s
+++ labs/lab4/lab4.s
@@ -11,44 +11,44 @@
 _start:
   // ----------------- SET DATA ------------------ //
   LDR X0, =dbA  // load the address of dbA in register X0
-  MOV X1, #100  // load immediate value of 100 into X1
+  MOV X1, #100  // mov immediate value of 100 into X1
   STR X1, [X0]  // store value of 100 in dbA
 
   LDR X0, =dbB  // load the address of dbB in register X0
-  MOV X1, #10000  // load immediate value of 10000 into X1
+  MOV X1, #10000  // mov immediate value of 10000 into X1
   STR X1, [X0]  // store value of 10000 in dbB
 
   LDR X0, =dbC  // load the address of dbC in register X0
-  MOV X1, #0x9680  // load immediate value of 10000000 into X1
-  STR X1, [X0]  // store value of 10000000 in dbC
+  MOV X1, #0x9680  // mov immediate value of 0x9680 into X1
+  STR X1, [X0]  // store value of 0x9680 in dbC
 
-  MOV X1, #0x98  // load immediate value of 10000000 into X1
-  STR X1, [X0, #2]  // store value of 10000000 in dbC
+  MOV X1, #0x98  // mov immediate value of 0x98 into X1
+  STR X1, [X0, #2]  // store value of 0x98 in dbC 2 bytes to the left
 
-  LDR X0, =dbD
-  MOV X1, #0xE400
-  STR X1, [X0]
+  LDR X0, =dbD  // load the address of dbD in register X0
+  MOV X1, #0xE400  // mov immediate value of 0xE400 into X1
+  STR X1, [X0]  // store value of 0xE400 in dbD
 
-  MOV X1, #0x540B
-  STR X1, [X0, #2]
+  MOV X1, #0x540B  // mov immediate value of 0x540B into X1
+  STR X1, [X0, #2]  // store value of 0x540B in dbD 2 bytes to the left
 
-  MOV X1, #0x2
-  STR X1, [X0, #4]
+  MOV X1, #0x2  // mov immediate value of 0x2 into X1
+  STR X1, [X0, #4]  // store value of 0x2 in dbD 4 bytes to the left
 
   // ----------------- SET DATA ------------------ //
 
   // ----------------- COMPUTE RESULT ------------------ //
-  LDR X0, =dbA
-  LDR X0, [X0]
+  LDR X0, =dbA  // load the address of dbA in register X0
+  LDR X0, [X0]  // store value of dbA in X0
 
-  LDR X1, =dbB
-  LDR X1, [X1]
+  LDR X1, =dbB  // load the address of dbB in register X1
+  LDR X1, [X1]  // store value of dbB in X1
 
-  LDR X2, =dbC
-  LDR X2, [X2]
+  LDR X2, =dbC  // load the address of dbC in register X2
+  LDR X2, [X2]  // store value of dbC in X2
 
-  LDR X3, =dbD
-  LDR X3, [X3]
+  LDR X3, =dbD  // load the address of dbD in register X3
+  LDR X3, [X3]  // store value of dbD in X3
 
   ADD X4, X0, X1  // X4 = dbA + dbB
   ADD X5, X2, X3  // X5 = dbC + dbD
