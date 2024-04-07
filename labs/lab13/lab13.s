4feb535946b315f2b41e8c6092316100524ed0b8
Carlos Aguilera <18aguilerac@gmail.com>
2024-04-07 01:36:40 +0000

    done with lab13

--- labs/lab13/lab13.s
+++ labs/lab13/lab13.s
@@ -7,20 +7,37 @@
 // set global start as the main entry
   .global _start
 
-  .equ BUFFER, 21
+  .equ buffer, 21
+  .equ dfd, -100
+  .equ mode, 0101
+  .equ permissions, 0660
 
   .data
-    chCr:       .byte   10
-    szString:   .asciz "cat in the hat"
+    szFilename: .asciz "output.txt"
+    szString:   .asciz "cat in the hat\n"
+
 
   .section .text
 
 _start:
-  LDR X0, =szString  // load the address of szPrompt into X0
-  BL putstring        // branch link to putstring
+  MOV X0, #dfd          // load the address of filename into X0
+  LDR X1, =szFilename   // load the address of filename into X0
+  MOV X2, #mode         // load mode into X0
+  MOV X3, #permissions  // give read write perms
+  MOV X8, #56           // sys call openat
+  SVC #0                // call sys
 
-  LDR X0, =chCr // load the address of szResult into X1
-  BL putch // branch link to putstring
+  MOV X29, X0  // store file descriptor
+
+  MOV X0, X29        // get file descriptor set it to second param of write sys call
+  LDR X1, =szString  // load the address of szString into X0
+  MOV X2, #15        // move the value of 15 into X2
+  MOV X8, #64        // sys call write
+  SVC #0             // call sys
+
+  MOV X0, X29    // get file descriptor
+  MOV X8, #0x39  // sys call save
+  SVC #0         // call sys
 
   MOV  X0, #0   // Setup the parameters to exit the program
   MOV  X8, #93  // and then call Linux to do it.
